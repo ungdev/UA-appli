bb52a8c059b461ba5676505b530175c11da0a476
Florent PERGOUD <florentpergoud@gmail.com>
2018-04-09 20:55:09 +0200

    TEST : Adding a side bar menu composant

--- ReactTests/client/src/App.js
+++ ReactTests/client/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import LeftBar from './LeftBar';
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
+
 class App extends Component {
+  state = {
+    collapsed: false
+  };
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}>
+            <LeftBar />
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }} />
+            <Content style={{ margin: '0 16px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                Bill is a cat.
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
       </div>
     );
   }
---
+++ ReactTests/client/src/LeftBar.jsx
@@ -0,0 +1,262 @@
+import React, { Component } from 'react';
+import { Menu, Icon } from 'antd';
+import Logo_UA from './logo_ua_2018.png';
+
+const SubMenu = Menu.SubMenu;
+
+class LeftBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <img src={Logo_UA} class="logo" />
+          <Menu.Item key="1">
+            <Icon type="home" />
+            <span>Accueil</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="user" />
+            <span>Mon compte</span>
+          </Menu.Item>
+          <SubMenu
+            key="3"
+            title={
+              <span>
+                <Icon type="trophy" />
+                <span>Les tournois</span>
+              </span>
+            }>
+            <SubMenu
+              key="sub3-1"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>CS-GO</span>
+                </span>
+              }>
+              <Menu.Item key="3-1-1">
+                <Icon type="share-alt" />
+                <span>Arbre</span>
+              </Menu.Item>
+              <Menu.Item key="3-1-2">
+                <Icon type="calendar" />
+                <span>Planning</span>
+              </Menu.Item>
+              <Menu.Item key="3-1-3">
+                <Icon type="team" />
+                <span>Équipes</span>
+              </Menu.Item>
+              <Menu.Item key="3-1-4">
+                <Icon type="profile" />
+                <span>Règlement</span>
+              </Menu.Item>
+              <Menu.Item key="3-1-5">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3-2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Lol (Pro)</span>
+                </span>
+              }>
+              <Menu.Item key="3-2-1">
+                <Icon type="fork" />
+                <span>Arbre</span>
+              </Menu.Item>
+              <Menu.Item key="3-2-2">
+                <Icon type="calendar" />
+                <span>Planning</span>
+              </Menu.Item>
+              <Menu.Item key="3-2-3">
+                <Icon type="team" />
+                <span>Équipes</span>
+              </Menu.Item>
+              <Menu.Item key="3-2-4">
+                <Icon type="profile" />
+                <span>Règlement</span>
+              </Menu.Item>
+              <Menu.Item key="3-2-5">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3-3"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Lol (Amateur)</span>
+                </span>
+              }>
+              <Menu.Item key="3-3-1">
+                <Icon type="fork" />
+                <span>Arbre</span>
+              </Menu.Item>
+              <Menu.Item key="3-3-2">
+                <Icon type="calendar" />
+                <span>Planning</span>
+              </Menu.Item>
+              <Menu.Item key="3-3-3">
+                <Icon type="team" />
+                <span>Équipes</span>
+              </Menu.Item>
+              <Menu.Item key="3-3-4">
+                <Icon type="profile" />
+                <span>Règlement</span>
+              </Menu.Item>
+              <Menu.Item key="3-3-5">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3-4"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Hearthstone</span>
+                </span>
+              }>
+              <Menu.Item key="3-4-1">
+                <Icon type="fork" />
+                <span>Arbre</span>
+              </Menu.Item>
+              <Menu.Item key="3-4-2">
+                <Icon type="calendar" />
+                <span>Planning</span>
+              </Menu.Item>
+              <Menu.Item key="3-4-3">
+                <Icon type="inbox" />
+                <span>Decks</span>
+              </Menu.Item>
+              <Menu.Item key="3-4-4">
+                <Icon type="profile" />
+                <span>Règlement</span>
+              </Menu.Item>
+              <Menu.Item key="3-4-5">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3-5"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Battle royal ?</span>
+                </span>
+              }>
+              <Menu.Item key="3-5-1">
+                <Icon type="fork" />
+                <span>Arbre</span>
+              </Menu.Item>
+              <Menu.Item key="3-5-2">
+                <Icon type="calendar" />
+                <span>Planning</span>
+              </Menu.Item>
+              <Menu.Item key="3-5-3">
+                <Icon type="team" />
+                <span>Équipes</span>
+              </Menu.Item>
+              <Menu.Item key="3-5-4">
+                <Icon type="profile" />
+                <span>Règlement</span>
+              </Menu.Item>
+              <Menu.Item key="3-5-5">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3-6"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Libre</span>
+                </span>
+              }>
+              <Menu.Item key="3-6-1">
+                <Icon type="calendar" />
+                <span>Planning prev</span>
+              </Menu.Item>
+              <Menu.Item key="3-6-2">Proposer un tournois</Menu.Item>
+              <Menu.Item key="3-6-3">
+                <Icon type="customer-service" />
+                <span>Contact</span>
+              </Menu.Item>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu
+            key="sub4"
+            title={
+              <span>
+                <Icon type="rocket" />
+                <span>Animations</span>
+              </span>
+            }>
+            <Menu.Item key="4-1">
+              <Icon type="calendar" />
+              <span>Planning scène</span>
+            </Menu.Item>
+            <Menu.Item key="4-2">
+              <Icon type="environment-o" />
+              <span>Plan UA</span>
+            </Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub5"
+            title={
+              <span>
+                <Icon type="shop" />
+                <span>Shopping</span>
+              </span>
+            }>
+            <SubMenu
+              key="sub4-1"
+              title={
+                <span>
+                  <Icon type="coffee" />
+                  <span>Buvette</span>
+                </span>
+              }>
+              <Menu.Item key="4-1-1">
+                <Icon type="form" />
+                <span>Carte</span>
+              </Menu.Item>
+              <Menu.Item key="4-1-2">
+                <Icon type="dashboard" />
+                <span>Attente</span>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub4-2"
+              title={
+                <span>
+                  <Icon type="skin" />
+                  <span>Magasin</span>
+                </span>
+              }>
+              <Menu.Item key="4-2-1">
+                <Icon type="book" />
+                <span>Catalogue</span>
+              </Menu.Item>
+            </SubMenu>
+          </SubMenu>
+          <Menu.Item key="5">
+            <Icon type="star-o" />
+            <span>Partenaires</span>
+          </Menu.Item>
+          <Menu.Item key="6">
+            <Icon type="copyright" />
+            <span>Mentions légales</span>
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+}
+export default LeftBar;
