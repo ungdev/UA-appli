067f3815420533d7e8c13d94e33fc8e6fd01967a
Fleuh <fl.haghenbeek@gmail.com>
2018-04-12 14:46:46 +0200

    FEATURE : Example of displaying teams

--- ReactTests/client/src/components/Teams.jsx
+++ ReactTests/client/src/components/Teams.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import { List, Divider } from 'antd'
+
 
 class Teams extends React.Component {
   constructor(props) {
@@ -15,7 +17,32 @@ class Teams extends React.Component {
   }
 
   render() {
-    console.log('render Teams :', this.props, this.state)
+    // to import with game filter through back end
+    const teams = [
+      {
+        team: 'Les Casseurs de Fion',
+        members: [
+          { nom: 'Exterminator', role: 'Entraîneur' },
+          { nom: 'Middleator', role: 'Tank' },
+          { nom: 'Jungleator', role: 'Jungle' },
+          { nom: 'ADCator', role: 'ADC' },
+          { nom: 'Supportator', role: 'Support' },
+          { nom: 'Topator', role: 'Top' }
+        ]
+      },
+      {
+        team: 'Les Mangecacas',
+        members: [
+          { nom: 'Entrainecaca', role: 'Entraîneur' },
+          { nom: 'Middlecaca', role: 'Tank' },
+          { nom: 'Junglecaca', role: 'Jungle' },
+          { nom: 'ADCcaca', role: 'ADC' },
+          { nom: 'Supportcaca', role: 'Support' },
+          { nom: 'Topcaca', role: 'Top' }
+        ]
+      },
+    ]
+    console.log('render Teams :', teams)
     let game = this.state.game
     switch (game) {
       case ('lolamateur'):
@@ -37,7 +64,30 @@ class Teams extends React.Component {
         game = 'Ce jeu ne fait pas partie de la liste des tournois'
     }
     return (
-      <div>{game}</div>
+      <div>
+        <h1>{game}</h1>
+
+        {teams.map((team) => (
+          <div>
+            <h2>{team.team}</h2>
+            <List
+              itemLayout="horizontal"
+              dataSource={team.members}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    title={<div>{item.nom}</div>}
+                    description={item.role}
+                  />
+                </List.Item>
+              )}
+            />
+            <Divider />
+          </div>
+        )
+
+        )}
+      </div>
     )
   }
 }
