3e9a4fbb28b5a6fc66d0eb1bb787c86208f26375
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2018-10-16 22:43:14 +0200

    fixed that f* sneaky merge conflict

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -66,54 +66,6 @@ class Home extends React.Component {
     return (
       <div>
         <Intro />
-<<<<<<< HEAD
-        <LoginModal
-          isOpen={this.state.loginModalOpened}
-          onClose={this.closeLoginModal}
-          onForgot={this.openForgotModal}
-        />
-        <ForgotModal isOpen={this.state.forgotModalOpened} onClose={this.closeForgotModal} />
-
-        <main className="a-home">
-          <div className="a-home__content">
-            <Category id="informations">Informations</Category>
-            <Informations />
-            <Button raised={true}>C'est parti !</Button>
-            <div className="a-home__map">
-              <iframe
-                height="320"
-                title="Google Maps"
-                src="https://maps.google.com/maps?q=UTT Arena&t=&z=17&ie=UTF8&iwloc=&output=embed"
-                frameBorder="0"
-                scrolling="no"
-                marginHeight="0"
-                marginWidth="0"
-              />
-
-              <p>Vous pouvez vous rendre à Troyes par le train ou par l'A5.</p>
-            </div>
-            <Category id="spotlights">Tournois</Category>
-            <Spotlights />
-          </div>
-          <div className="a-home__content a-home__footer">
-            <div>
-              © UTT Net Group
-              <a href="/mentions">Mentions légales</a>
-            </div>
-            <div>
-              <a href={`mailto:${process.env.REACT_APP_CONTACT_MAIL}`}>
-                {process.env.REACT_APP_CONTACT_MAIL}
-              </a>
-            </div>
-            <div>
-              <a href={`tel:${process.env.REACT_APP_CONTACT_PHONE}`}>
-                {process.env.REACT_APP_CONTACT_PHONE}
-              </a>
-            </div>
-          </div>
-        </main>
-=======
->>>>>>> newLoginForm
       </div>
     )
   }
