2ce293e102f1dc3b8a9958373059361eb7d0f379
Lyght <valentinpeltier@yahoo.fr>
2018-10-24 23:42:04 +0200

    Adding twitter

--- src/containers/dashboard/components/Accueil.jsx
+++ src/containers/dashboard/components/Accueil.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 class Accueil extends React.Component {
   render() {
     return (
-      <div style={{ height: '100%'}} ref={el => (this.instance = el)}>
+      <div style={{ height: '100%'}}>
         <h1>Accueil</h1>
         
-        <iframe className="facebook" src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FUTTArena&width=500&height=700&stream=true" />
+        <h2>Derniers posts</h2>
+
+        <div className="social-embed">
+          <div>
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUTTArena&tabs=timeline&width=500&height=700&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="500" height="700" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" style={{ border: 'none', overflow: 'hidden'}}></iframe>
+          </div>
+
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="uttarena"
+            lang="fr"
+            noFooter="true"
+            options={{height: 700, width: 500 }} />
+        </div>
       </div>
     );
   }
