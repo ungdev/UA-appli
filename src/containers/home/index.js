2adcfdddc7155c8e2454c2d93afd5a3383582de3
Lyght <valentinpeltier@yahoo.fr>
2018-10-27 17:26:35 +0200

    Update login style and fixes

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -10,52 +10,9 @@ import { autoLogin } from "../../modules/login"
 import { push } from "react-router-redux"
 
 class Home extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      loginModalOpened: false,
-      forgotModalOpened: false
-    }
-
-    // this.openLoginModal = this.openLoginModal.bind(this)
-    // this.openForgotModal = this.openForgotModal.bind(this)
-    // this.closeLoginModal = this.closeLoginModal.bind(this)
-    // this.closeForgotModal = this.closeForgotModal.bind(this)
-    this.scrollCapture = this.scrollCapture.bind(this)
-    this.mainButon = this.mainButon.bind(this)
-  }
-
   componentWillMount() {
     this.props.fetchCanLogin()
     this.props.autoLogin()
-
-    document.addEventListener("scroll", this.scrollCapture, { passive: true })
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener("scroll", this.scrollCapture)
-  }
-
-  scrollCapture() {
-    const scrollTop =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop ||
-      0
-
-    const bottom = window.innerHeight + 90 - 12
-
-    document.body.className =
-      scrollTop >= document.body.scrollHeight - bottom ? "a-social-fixed" : ""
-  }
-
-  mainButon() {
-    if (this.props.isLoggedIn) {
-      this.props.gotoDashboard()
-    } else {
-      this.openLoginModal()
-    }
   }
 
   render() {
