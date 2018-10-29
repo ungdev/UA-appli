db303876ab9e6e8c5b8bd8a71ca40c6aa724a4dc
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-29 19:38:55 +0100

    tweeks on leftbar

--- src/containers/dashboard/components/LeftBar.jsx
+++ src/containers/dashboard/components/LeftBar.jsx
@@ -20,6 +20,7 @@ class LeftBar extends React.Component {
         openKeys.push('2')
         if(tab[2] === 'users') current = `2-1`
         if(tab[2] === 'paids') current = `2-2`
+        if(tab[2] === 'material') current = `2-4`
         if(tab.length >= 2 && tab[2] === 'spotlights'){
           openKeys.push('2-3')
           current = `2-3-${tab[3]}`
@@ -222,62 +223,6 @@ class LeftBar extends React.Component {
             </Menu.Item>
           </SubMenu>
         </SubMenu>
-        <SubMenu
-          key="4"
-          title={
-            <span>
-              <Icon type="rocket" />
-              <span>Animations</span>
-            </span>
-          }>
-          <Menu.Item key="4-1">
-            <Icon type="calendar" />
-            <span>Planning scène</span>
-          </Menu.Item>
-          <Menu.Item key="4-2">
-            <Icon type="environment-o" />
-            <span>Plan UA</span>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu
-          key="5"
-          title={
-            <span>
-              <Icon type="shop" />
-              <span>Shopping</span>
-            </span>
-          }>
-          <SubMenu
-            key="5-1"
-            title={
-              <span>
-                <Icon type="coffee" />
-                <span>Buvette</span>
-              </span>
-            }>
-            <Menu.Item key="5-1-1">
-              <Icon type="form" />
-              <span>Carte</span>
-            </Menu.Item>
-            <Menu.Item key="5-1-2">
-              <Icon type="dashboard" />
-              <span>Attente</span>
-            </Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="5-2"
-            title={
-              <span>
-                <Icon type="skin" />
-                <span>Magasin</span>
-              </span>
-            }>
-            <Menu.Item key="5-2-1">
-              <Icon type="book" />
-              <span>Catalogue</span>
-            </Menu.Item>
-          </SubMenu>
-        </SubMenu>
         <Menu.Item key="6">
           <a href="https://arena.utt.fr/partners">
             <Icon type="star-o" />
@@ -285,8 +230,10 @@ class LeftBar extends React.Component {
           </a>
         </Menu.Item>
         <Menu.Item key="7">
-          <Icon type="copyright" />
-          <span>Mentions légales</span>
+          <a href="https://arena.utt.fr/mentions-legales">
+            <Icon type="copyright" />
+            <span>Mentions légales</span>
+          </a>
         </Menu.Item>
       </Menu>
     )
