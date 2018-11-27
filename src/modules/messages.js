18ac64b94e21301c61bf798313cfb99cc7814c5a
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2018-11-27 17:36:54 +0100

    set timer to send messages

--- src/modules/messages.js
+++ src/modules/messages.js
@@ -1,8 +1,11 @@
 import axios from '../lib/axios'
 import { actions as notifActions } from 'redux-notifications'
+import moment from 'moment'
+
 
 export const SET_MESSAGES = 'messages/SET_MESSAGES'
 export const SET_MESSAGES_LOADING = 'messages/SET_MESSAGES'
+export const SET_LAST_MESSAGE_TIME = 'messages/LAST_MESSAGE_TIME'
 
 const initialState = {
   messages: [],
@@ -18,7 +21,7 @@ export default (state = initialState, action) => {
       //test has new elements
       messages = state.messages.splice(0) // peut poser probleme de splice
       if (!messages) messages = []
-      action.payload.forEach((message) => {
+      action.payload.forEach(message => {
         let found = messages.find(i => message.id === i.id) // if we find a matching spotlight
         if (!found) messages.push(message) //we do not add it to the tab
       })
@@ -26,6 +29,8 @@ export default (state = initialState, action) => {
       return { messages: messages, loading: false }
     case SET_MESSAGES_LOADING:
       return { ...state, loading: true }
+    case SET_LAST_MESSAGE_TIME:
+      return { ...state, lastMessageTime: action.payload }
     default:
       return state
   }
@@ -35,25 +40,27 @@ export const fetchMessages = () => {
   return async (dispatch, getState) => {
     const authToken = getState().login.token
     if (!authToken || authToken.length === 0) return
-    const messages = await axios.get(`messages`, { headers: { 'X-Token': authToken } })
+    const messages = await axios.get(`messages`, {
+      headers: { 'X-Token': authToken }
+    })
     dispatch({
       type: SET_MESSAGES,
       payload: messages.data
     })
-    
   }
 }
 
-export const fetchMessagesByIdUser = (idTo) => {
+export const fetchMessagesByIdUser = idTo => {
   return async (dispatch, getState) => {
     const authToken = getState().login.token
     if (!authToken || authToken.length === 0) return
-    const messages = await axios.get(`messages/${idTo}`, { headers: { 'X-Token': authToken } })
+    const messages = await axios.get(`messages/${idTo}`, {
+      headers: { 'X-Token': authToken }
+    })
     dispatch({
       type: SET_MESSAGES,
       payload: messages.data
     })
-    
   }
 }
 
@@ -62,8 +69,28 @@ export const sendMessage = (to, message, spotlight) => {
     const authToken = getState().login.token
     if (!authToken || authToken.length === 0) return
     try {
-      const res = await axios.post(`messages`, { to, message, spotlight }, { headers: { 'X-Token': authToken } })
-      if(res.status === 200) {
+      const { lastMessageTime } = getState().messages
+      console.log(getState().payload)
+      if(lastMessageTime && lastMessageTime + 30000 > moment().valueOf()){
+        const diff = Math.ceil((lastMessageTime + 30000 - moment().valueOf()) / 1000)
+        return dispatch(
+          notifActions.notifSend({
+            message: `Veuillez attendre encore ${diff} secondes avant de renvoyer un message`,
+            kind: 'danger',
+            dismissAfter: 2000
+          })
+        )
+      }
+      dispatch({
+        type: SET_LAST_MESSAGE_TIME,
+        payload: moment().valueOf()
+      })
+      const res = await axios.post(
+        `messages`,
+        { to, message, spotlight },
+        { headers: { 'X-Token': authToken } }
+      )
+      if (res.status === 200) {
         dispatch(
           notifActions.notifSend({
             message: 'Message envoyé avec succès',
@@ -71,8 +98,7 @@ export const sendMessage = (to, message, spotlight) => {
           })
         )
       }
-    }
-    catch (e){
+    } catch (e) {
       console.log(e)
       dispatch(
         notifActions.notifSend({
@@ -82,9 +108,5 @@ export const sendMessage = (to, message, spotlight) => {
         })
       )
     }
-    
   }
 }
-
-
-
