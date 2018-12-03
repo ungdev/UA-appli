9ffb920bcbaff3111bc9dea9dd1e856863f59cf8
Lyght <valentinpeltier@yahoo.fr>
2018-12-03 22:39:16 +0100

    Remove facebook and twitter embed from home

--- src/containers/dashboard/components/Accueil.jsx
+++ src/containers/dashboard/components/Accueil.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { TwitterTimelineEmbed } from 'react-twitter-embed'
 import { connect } from 'react-redux'
 import { Card, Spin } from 'antd'
 import { fetchUser } from '../../../modules/user'
-//import qs from 'qs'
 
 const colorResult = (result) => {
   switch (result) {
@@ -105,24 +103,6 @@ class Accueil extends React.Component {
           <div>Place : <strong>{user.place ? user.place : 'Aucune'}{user.plusone ? ' (Visiteur)' : ''}</strong></div>
           {user.team && user.team.spotlight ? <div>Tournoi : <strong>{user.team.spotlight.name}</strong></div> : ''}
           {user.team && !user.team.soloTeam ? <div>Équipe : <strong>{user.team.name}</strong></div> : ''}
-        </Card>
-
-        <Card
-          title="Derniers posts"
-        >
-          <div className="social-embed">
-            <div>
-              <iframe title="Facebook UTTArena" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUTTArena&tabs=timeline&width=500&height=700&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="500" height="700" scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media" style={{ border: 'none', overflow: 'hidden'}}></iframe>
-            </div>
-
-            <TwitterTimelineEmbed
-              sourceType="profile"
-              screenName="uttarena"
-              lang="fr"
-              noFooter={true}
-              options={{ height: 700, width: 500 }}
-            />
-          </div>
         </Card>
       </div>
     )
