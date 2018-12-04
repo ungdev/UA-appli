20c1ead8639c336d59378c215eff2de972de6d83
Arnaud9145 <arnaud.dufour@utt.fr>
2018-12-04 19:30:09 +0100

    added libre calendar

--- src/containers/dashboard/components/Libre/Calendar.jsx
+++ src/containers/dashboard/components/Libre/Calendar.jsx
@@ -10,7 +10,7 @@ class Calendar extends React.Component {
     return (
       <div>
         <Divider />
-        <iframe title="libreCalendar" src="https://calendar.google.com/calendar/b/1/embed?showPrint=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=4mdfsfsmm9m4jbap8qlht0opao%40group.calendar.google.com&amp;color=%238D6F47&amp;ctz=Europe%2FParis" width="800" height="600" scrolling="no"></iframe>
+        <iframe title="libreCalendar" src="https://calendar.google.com/calendar/b/1/embed?mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%2333ccff&amp;src=1141enfov97obfao4ergs9g6f4%40group.calendar.google.com&amp;color=%23AB8B00&amp;ctz=Europe%2FParis" width="800" height="600" scrolling="no"></iframe>
       </div>
     )
   }
