714f4def42f071c87ea9f8b3527662765a67593d
florianess <florian.esslinger@live.fr>
2018-12-04 19:47:45 +0100

    Remove facebook and twitter feed

--- src/containers/dashboard/components/Accueil.jsx
+++ src/containers/dashboard/components/Accueil.jsx
@@ -124,26 +124,6 @@ class Accueil extends React.Component {
         <div style={{ display: 'flex' }}>
           {this.getMatches()}
         </div>
-
-        <Divider />
-
-        <Card
-          title="Derniers posts"
-        >
-          <div className="social-embed">
-            <div>
-              <iframe title="Facebook UTTArena" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUTTArena&tabs=timeline&width=500&height=700&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="500" height="700" scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media" style={{ border: 'none', overflow: 'hidden'}}></iframe>
-            </div>
-
-            <TwitterTimelineEmbed
-              sourceType="profile"
-              screenName="uttarena"
-              lang="fr"
-              noFooter={true}
-              options={{ height: 700, width: 500 }}
-            />
-          </div>
-        </Card>
       </div>
     )
   }
